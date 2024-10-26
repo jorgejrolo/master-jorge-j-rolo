d892f95250618499b858ab67a3b7121da1b00263
Jorge J. Rolo <hola@jorgejrolo.com>
2024-10-26 15:10:25 +0200

    Instalación WP por medio de MAMP Pro

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<h1> Esto es PHP </h1>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-<?php echo date("m.d.y"); ?>
-<?php echo "Hola Mundo"; ?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
